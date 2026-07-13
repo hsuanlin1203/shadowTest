e5daa319af9211e53ef4b58ffc0d67423c0f8126
hsuanlin1203 <wayne.lin1203@gmail.com>
2026-07-13 22:57:50 +0800

    fix: exclude ES autoconfiguration globally, not just in test

--- src/test/java/com/example/shadowtest/ShadowTestApplicationTests.java
+++ src/test/java/com/example/shadowtest/ShadowTestApplicationTests.java
@@ -3,11 +3,7 @@ package com.example.shadowtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-    "elasticsearch.host=localhost",
-    "elasticsearch.port=9200",
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration"
-})
+@SpringBootTest
 class ShadowTestApplicationTests {
     @Test
     void contextLoads() {
